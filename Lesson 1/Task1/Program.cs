05594299ba87537272db6025e5b4d5fabad0b802
Антонина Акимова <antoninaakimova@MacBook-Air-Antonina.local>
2024-02-21 22:13:57 +0700

    add some variables

--- Lesson 1/Task1/Program.cs
+++ Lesson 1/Task1/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
+
+
+int value = 100;
+Console.WriteLine(value);
