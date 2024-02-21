1013f2f38e1ddd07451a3a429cd54f5c8608ce68
Антонина Акимова <antoninaakimova@MacBook-Air-Antonina.local>
2024-02-21 22:50:14 +0700

    add Task2

---
+++ Lesson 1/Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = 5;
+int b = 6;
+int sum = a + b;
+Console.WriteLine(sum);
+
+long c = 7;
+long sum2 = a + c;
+Console.WriteLine(sum2);
+
+double j = 9.2;
+double sum3 = c + j;
+Console.WriteLine(sum3);
