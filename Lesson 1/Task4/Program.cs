d9e6540f9b43fa4500375b56de276073b78f9e0c
Антонина Акимова <antoninaakimova@MacBook-Air-Antonina.local>
2024-02-22 14:22:34 +0700

    add Task4

---
+++ Lesson 1/Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 5;
+int b = 8;
+int c = 9;
+int e = 12;
+int d = 25;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (e > max)
+{
+    max = e;
+}
+if (d > max)
+{
+    max = d;
+}
+Console.WriteLine(max);
