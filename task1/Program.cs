3dc366044fcd45b571509360e1dcff294ec7d0b5
Антонина Акимова <antoninaakimova@MacBook-Air-Antonina.local>
2024-03-07 13:42:20 +0700

    add task1

---
+++ task1/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] arr = new int[5];
+arr[0] = 5;
+arr[1] = 7;
+arr[2] = 3;
+arr[3] = 8;
+arr[4] = 4;
+
+Console.WriteLine(arr[0]);
+Console.WriteLine(arr[11]);
+Console.WriteLine(arr[2]);
+Console.WriteLine(arr[3]);
+Console.WriteLine(arr[4]);
+
+//Console.WriteLine(arr [5]);
+int[] arr2 = new int[5] { 1, 2, 3, 4, 5 };
+
+int[] arr3 = { 1, 2, 3, 4, 5, 7 };
+// arr = [1,2,3,4,5]
